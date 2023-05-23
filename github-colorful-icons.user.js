ced06305171603c4f32c5d8b65f873a7d49d7851
Rex <vivelarepublique@qq.com>
2023-05-23 16:29:15 +0800

    initialization

---
+++ github-colorful-icons.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         github colorful icons
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       vivelarepublique
+// @match        https://github.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+})();
